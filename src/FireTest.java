5b5012e97b3fcf17bfbc15bafba9db3d520a4820
Auberon López <auberonedu@gmail.com>
2025-04-22 12:25:29 -0700

    Example test case

---
+++ src/FireTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FireTest {
+    @Test
+    public void testTimeToBurnExample() {
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 2;
+        int matchC = 6;
+
+        int expected = 8;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
+}
